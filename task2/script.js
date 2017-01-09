243ac6a7be17648f4ad663a20cad7dd7deb79aa9
Slav-Alexander Mitov <slav.alexandarm@gmail.com>
2017-01-09 21:45:01 +0200

    Slav Mitov
    Final Test

--- task2/script.js
+++ task2/script.js
@@ -3,16 +3,23 @@
 // задача 1
 
 var arr = [2, 5, 8, 4, 1, 12];
+arr.sort(function(a,b){return a-b});
 
 // задача 2
 
 var text = "The quick brown fox jumps over the lazy dog";
+var list = text.split(" ");
 
+function words(list) {
+	return list.length >= 4;
+}
 // задача 3
 
 
 // задача 4
 
-
 // задача 5
+if (document.cookie.indexOf('sessionKey') > -1 ) {
+  alert("Cookie exists!");
+}
 
